900738668b7422471aef7bb051ea1dfb10eb2fa6
Anupam Basak <anupam.basak27@gmail.com>
2018-03-13 16:10:52 +0530

    More Fixes

--- nxos-repository-util.sh
+++ nxos-repository-util.sh
@@ -3,26 +3,25 @@
 function createAmd64Mirrors() {
   echo "- CREATING REPOSITORY MIRRORS"
 
-  echo
-  echo "\t- Adding Key for Ubuntu Xenial"
-  gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32
-  echo "\t- Creating AMD64 Mirror for Ubuntu Xenial : xenial"
+  echo "  - Adding Key for Ubuntu Xenial"
+  gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32 > /dev/null 2>&1
+  echo "  - Creating AMD64 Mirror for Ubuntu Xenial : xenial"
   aptly mirror create -architectures="amd64" xenial http://archive.ubuntu.com/ubuntu xenial main universe multiverse restricted
 
-  echo "\t- Creating AMD64 Mirror for Ubuntu Xenial - Updates : xenial-updates"
+  echo "  - Creating AMD64 Mirror for Ubuntu Xenial - Updates : xenial-updates"
   aptly mirror create -architectures="amd64" xenial-updates http://archive.ubuntu.com/ubuntu xenial-updates main universe multiverse restricted
 
-  echo "\t- Creating AMD64 Mirror for Ubuntu Xenial - Security : xenial-security"
+  echo "  - Creating AMD64 Mirror for Ubuntu Xenial - Security : xenial-security"
   aptly mirror create -architectures="amd64" xenial-security http://archive.ubuntu.com/ubuntu xenial-security main universe multiverse restricted
 
   echo
-  echo "\t- Adding Key for KDENeon Xenial"
-  gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys E6D4736255751E5D
-  echo "\t- Creating AMD64 Mirror for KDENeon Xenial : kdeneon-xenial"
+  echo "  - Adding Key for KDENeon Xenial"
+  gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys E6D4736255751E5D > /dev/null 2>&1
+  echo "  - Creating AMD64 Mirror for KDENeon Xenial : kdeneon-xenial"
   aptly mirror create -architectures="amd64" kdeneon-xenial https://archive.neon.kde.org/user/ xenial main
 
   echo
-  echo "\t- Creating AMD64 Mirror for NXOS : nxos"
+  echo "  - Creating AMD64 Mirror for NXOS : nxos"
   aptly mirror create -ignore-signatures=true -architectures="amd64" nxos http://repo.nxos.org/ nxos main  
 }
 
