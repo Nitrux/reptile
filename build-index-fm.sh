4719567866eb1465379629f4b571cb4c757ed7af
bruno queiros <bqueiros@gmail.com>
2019-08-10 16:58:33 +0100

    remove sudo from build-index-fm.sh

--- build-index-fm.sh
+++ build-index-fm.sh
@@ -21,7 +21,9 @@ wget -Nc https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdep
 
 chmod +x linuxdeploy*
 
-apt-get install ecm qtbase5-dev build-essential git gcc g++ qtdeclarative5-dev qml-module-qtquick-controls libqt5svg5-dev qtmultimedia5-dev automake cmake qtquickcontrols2-5-dev libkf5config-dev libkf5service-dev libkf5notifications-dev libkf5kiocore5 libkf5kio-dev qml-module-qtwebengine gettext extra-cmake-modules libkf5wallet-dev qtbase5-private-dev qtwebengine5-dev libkf5wallet-dev qt5-default qt5-default libqt5websockets5-dev libtag1-dev libkf5people-dev libkf5contacts-dev -y
+whoami
+
+sudo apt-get install ecm qtbase5-dev build-essential git gcc g++ qtdeclarative5-dev qml-module-qtquick-controls libqt5svg5-dev qtmultimedia5-dev automake cmake qtquickcontrols2-5-dev libkf5config-dev libkf5service-dev libkf5notifications-dev libkf5kiocore5 libkf5kio-dev qml-module-qtwebengine gettext extra-cmake-modules libkf5wallet-dev qtbase5-private-dev qtwebengine5-dev libkf5wallet-dev qt5-default qt5-default libqt5websockets5-dev libtag1-dev libkf5people-dev libkf5contacts-dev -y
 
 [ ! -d "kirigami" ] && git clone $KIRIGAMI_SRCS && [ -d "kirigami" ] && git pull origin master
 pushd ./kirigami && [ ! -d "build" ] && mkdir build
