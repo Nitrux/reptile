87d9a495893673ac4112a9117ae9ae781c4202fe
bruno queiros <bqueiros@gmail.com>
2019-08-10 17:36:53 +0100

    new run execution

--- build-index-fm.sh
+++ build-index-fm.sh
@@ -1,39 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-LINUXDEPLOY_TOOL=$PWD/linuxdeploy-tool
+# Install linuxdeploy
+LINUXDEPLOY_BIN=$PWD/linuxdeploy
+LINUXDEPLOY_PLUGIN_QT_BIN=$PWD/linuxdeploy-plugin-qt
 
-#Sources
-KIRIGAMI_SRCS=git://anongit.kde.org/kirigami
-MAUIKIT_SRCS=git://anongit.kde.org/mauikit
-INDEX_SRCS=git://anongit.kde.org/index-fm
-
-wget -Nc https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage -O $LINUXDEPLOY_TOOL
+wget -Nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O $LINUXDEPLOY_BIN
+wget -Nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage -O $LINUXDEPLOY_PLUGIN_QT_BIN
 
 chmod +x linuxdeploy*
 
+# Builds config
+BUILD_TYPE=Release
+INSTALL_PREFIX=$PWD/usr
+
+# Sources
+KIRIGAMI_SRCS=git://anongit.kde.org/kirigami
+MAUIKIT_SRCS=https://invent.kde.org/kde/mauikit
+
 sudo apt-get install ecm qtbase5-dev build-essential git gcc g++ qtdeclarative5-dev qml-module-qtquick-controls libqt5svg5-dev qtmultimedia5-dev automake cmake qtquickcontrols2-5-dev libkf5config-dev libkf5service-dev libkf5notifications-dev libkf5kiocore5 libkf5kio-dev qml-module-qtwebengine gettext extra-cmake-modules libkf5wallet-dev qtbase5-private-dev qtwebengine5-dev libkf5wallet-dev qt5-default qt5-default libqt5websockets5-dev libtag1-dev libkf5people-dev libkf5contacts-dev -y
 
-[ ! -d "kirigami" ] && git clone $KIRIGAMI_SRCS "kirigami" && [ -d "kirigami" ] && git pull origin master
-pushd ./kirigami && [ ! -d "build" ] && mkdir build
-cd ./build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-sudo make install
-
+[ ! -d "kirigami" ] && git clone "$KIRIGAMI_SRCS" "kirigami" --depth 1  && [ -d "kirigami" ]
+pushd "./kirigami" && [ ! -d "build" ] && mkdir build
+    cd ./build
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+    sudo make install
 popd
-[ ! -d "mauikit" ] && git clone $MAUIKIT_SRCS "mauikit" && [ -d "mauikit" ] && git pull origin master
-pushd ./mauikit && [ ! -d "build" ] && mkdir build
-cd ./build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-sudo make install
 
+
+[ ! -d "mauikit" ] && git clone "$MAUIKIT_SRCS"  --depth 1 && [ -d "mauikit" ]
+    pushd ./mauikit && [ ! -d "build" ] && mkdir build
+    cd ./build
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+    sudo make install
 popd
-[ ! -d "index-fm" ] && git clone $INDEX_SRCS "index-fm" && [ -d "index-fm" ] && git pull origin master
+
+
+[ ! -d "index-fm" ] && git clone https://invent.kde.org/kde/index-fm --depth 1 && [ -d "index-fm" ]
 pushd ./index-fm && [ ! -d "build" ] && mkdir build
 cd ./build
-cmake .. -DCMAKE_INSTALL_PREFIX=./AppDir
-sudo make install
-cp ../src/index.png ./system-file-manager.png
-find $HOME/build-*-*_Qt_* \( -name "moc_*" -or -name "*.o" -or -name "qrc_*" -or -name "Makefile*" -or -name "*.a" \) -exec rm {} \;
-# $1 ./AppDir/share/applications/org.kde.index.desktop -appimage -unsupported-allow-new-glibc -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/org/kde/kirigami.2 -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/org/kde/mauikit -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Controls.2/org.kde.desktop -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QMLTermWidget 
+cmake  -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" ..
 
-sudo $LINUXDEPLOY_TOOL ./AppDir/share/applications/org.kde.index.desktop -appimage -unsupported-allow-new-glibc -extra-plugins=iconengines -qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Controls -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Templates.2 -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Controls.2 -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Layouts -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/org/kde/kirigami.2 -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/org/kde/mauikit -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QtQuick/Controls.2/org.kde.desktop -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/QMLTermWidget
+[ -d "AppDir" ] && rm -rf AppDir
+DESTDIR=AppDir make install -j`nproc`
+
+# The desktop file entry should set the right file icon name without the file extension
+sed -i "s/Icon=.*/Icon=index/g" AppDir/usr/share/applications/org.kde.index.desktop
+
+
+$LINUXDEPLOY_BIN --appdir=AppDir
+
+export QML_MODULES_PATHS=/usr/lib/x86_64-linux-gnu/qt5/qml:$INSTALL_PREFIX/lib/x86_64-linux-gnu/qml
+export QML_SOURCES_PATHS=$PWD/../src/
+$LINUXDEPLOY_PLUGIN_QT_BIN --appdir=AppDir
+
+$LINUXDEPLOY_BIN --appdir=AppDir --output appimage
+exit 0
+Collapse
+
